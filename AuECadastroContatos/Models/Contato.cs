c83af65a84d7bd910672cb508d2f38755d8fab80
brunophg <brunophgjf@gmail.com>
2026-07-29 09:18:06 -0300

    chore: configuracao inicial da arquitetura e importacao do banco auebd.mdb

---
+++ AuECadastroContatos/Models/Contato.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuECadastroContatos.Models
+{
+    internal class Contato
+    {
+    }
+}
